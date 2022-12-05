0a259e3833db05b4d2903d5ea73f61f0502ebc95
Anna Vasilchenko <annavasilcenko482@gmail.com>
2022-12-05 15:09:37 +0300

    Добавляет домащние задания

---
+++ array_random/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задает массив из 8-ми элементов и выводит  их на экран
+
+int [] array = new int [8];
+
+void PushArray(int [] array)
+{
+  for(int i=0; i<array.Length; i++)
+  {
+    array[i] = new Random().Next(0, 1000);
+  }
+}
+void PrintArray(int [] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.WriteLine(array[i]);
+  }
+}
+PushArray(array);
+PrintArray(array);
---
+++ num_pow/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и В) и возводит число А в натуральную степень В
+// 3,5->243
+// 2,4->16
+
+void NumPow (int number, int degree)
+{
+  int result =1;
+  for(int i=1; i<=degree; i++)
+  {
+    result= result*number; 
+  }
+  Console.WriteLine(result);
+}
+
+Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень числа: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+NumPow(number, degree);
+
